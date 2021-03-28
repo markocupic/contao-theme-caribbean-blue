adaf1a25cca7e23df904718c1bb01792085de1d2
Marko Cupic <m.cupic@gmx.ch>
2021-03-28 09:59:08 +0200

    remove popper dependency

--- system/modules/theme-caribbean-blue/config/config.php
+++ system/modules/theme-caribbean-blue/config/config.php
@@ -2,8 +2,7 @@
 if (TL_MODE == 'FE')
 {
     $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-caribbean-blue/js/theme.js|static';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.min.js';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.bundle.min.js|static';
     //$GLOBALS['TL_JAVASCRIPT'][] = 'https://use.fontawesome.com/926b4fc2c0.js';
     $GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
     $GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css';
