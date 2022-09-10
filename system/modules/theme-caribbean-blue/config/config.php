584de6bec30930ebad1bb66bbc82cd7f5fcc5106
Marko Cupic <m.cupic@gmx.ch>
2022-09-10 12:43:40 +0200

    Rewrite the navbar script to vanilla.js

--- system/modules/theme-caribbean-blue/config/config.php
+++ system/modules/theme-caribbean-blue/config/config.php
@@ -2,6 +2,7 @@
 if (TL_MODE == 'FE')
 {
     $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-caribbean-blue/js/theme.js|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-caribbean-blue/js/navbar_navigation.js|static';
     $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.bundle.min.js|static';
     //$GLOBALS['TL_JAVASCRIPT'][] = 'https://use.fontawesome.com/926b4fc2c0.js';
     $GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
