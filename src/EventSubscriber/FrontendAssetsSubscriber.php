a468fd99ffb3b45b25e577be655a855bd1770545
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-09-14 20:05:46 +0200

    Hotfix

--- src/EventSubscriber/FrontendAssetsSubscriber.php
+++ src/EventSubscriber/FrontendAssetsSubscriber.php
@@ -28,11 +28,15 @@ class FrontendAssetsSubscriber implements EventSubscriberInterface
         if ($this->scopeMatcher->isFrontendRequest($request)) {
             $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-caribbean-blue/js/theme.js|static';
             $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-caribbean-blue/js/navbar_navigation.js|static';
-            $GLOBALS['TL_BODY'][] = 'assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.bundle.min.js';
-            //$GLOBALS['TL_JAVASCRIPT'][] = 'https://use.fontawesome.com/926b4fc2c0.js';
+
+            // Bootstrap
+            $GLOBALS['TL_BODY'][] = '<script defer src="/assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.bundle.min.js"></script>';
+
+            // WOW
             $GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
             $GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css';
 
+            // Font Awesome
             $GLOBALS['TL_CSS'][] = '/files/fontawesome-pro-5.5.0-web/css/all.css|static';
             $GLOBALS['TL_BODY'][] = '<script defer src="/files/fontawesome-pro-5.5.0-web/js/all.js"></script>';
         }
