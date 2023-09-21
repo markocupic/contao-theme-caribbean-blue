cbc61aae0a0ceace00f6d007b7fe7a9fc1d75a4f
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-09-21 19:56:55 +0200

    Import Nunito via the link tag in the head

--- src/EventSubscriber/FrontendAssetsSubscriber.php
+++ src/EventSubscriber/FrontendAssetsSubscriber.php
@@ -40,6 +40,9 @@ class FrontendAssetsSubscriber implements EventSubscriberInterface
             $GLOBALS['TL_JAVASCRIPT'][] = 'files/themes/theme-caribbean-blue/js/navbar_navigation.js|static';
             //$GLOBALS['TL_CSS'][] = 'files/themes/theme-caribbean-blue/css/main.css|static';
 
+            // Google web fonts
+            $GLOBALS['TL_HEAD'][] = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">';
+
             // Bootstrap
             $GLOBALS['TL_BODY'][] = '<script defer src="/assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.bundle.min.js"></script>';
 
