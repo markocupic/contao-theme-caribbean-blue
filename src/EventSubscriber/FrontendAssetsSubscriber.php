f76d15947e30234446ad28f9e128e209c0898379
Marko Cupic <m.cupic@gmx.ch>
2026-06-06 11:17:26 +0200

    Apply the new logo

--- src/EventSubscriber/FrontendAssetsSubscriber.php
+++ src/EventSubscriber/FrontendAssetsSubscriber.php
@@ -17,19 +17,21 @@ namespace Markocupic\ContaoThemeCaribbeanBlue\EventSubscriber;
 use Contao\CoreBundle\Routing\ResponseContext\Csp\CspHandler;
 use Contao\CoreBundle\Routing\ResponseContext\ResponseContextAccessor;
 use Contao\CoreBundle\Routing\ScopeMatcher;
+use Contao\LayoutModel;
+use Contao\PageModel;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class FrontendAssetsSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly Packages                $packages,
+        private readonly Packages $packages,
         private readonly ResponseContextAccessor $responseContextAccessor,
-        private readonly ScopeMatcher            $scopeMatcher,
-    )
-    {
+        private readonly ScopeMatcher $scopeMatcher,
+    ) {
     }
 
     public static function getSubscribedEvents()
@@ -41,12 +43,14 @@ class FrontendAssetsSubscriber implements EventSubscriberInterface
     {
         $request = $e->getRequest();
 
-        if ($this->scopeMatcher->isFrontendRequest($request)) {
+        $templateName = $this->getTemplateName($request);
+
+        if (str_starts_with((string) $templateName, 'fe_page_caribbean_blue') && $this->scopeMatcher->isFrontendRequest($request)) {
             $GLOBALS['TL_JAVASCRIPT'][] = $this->packages->getUrl('js/theme/theme.js', 'markocupic_contao_theme_caribbean_blue');
 
             // Bootstrap
             $GLOBALS['TL_CSS'][] = $this->packages->getUrl('styles/frontend.css', 'markocupic_contao_theme_caribbean_blue');
-            $GLOBALS['TL_BODY'][] = '<script defer src="' . $this->packages->getUrl('bootstrap/dist/js/bootstrap.bundle.min.js', 'markocupic_contao_theme_caribbean_blue') . '"></script>';
+            $GLOBALS['TL_BODY'][] = '<script defer src="'.$this->packages->getUrl('bootstrap/dist/js/bootstrap.bundle.min.js', 'markocupic_contao_theme_caribbean_blue').'"></script>';
 
             // Viewport
             $GLOBALS['TL_HEAD'][] = '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
@@ -68,22 +72,22 @@ class FrontendAssetsSubscriber implements EventSubscriberInterface
     private function getFaviconMarkup(): string
     {
         return '
-      <link rel="apple-touch-icon" sizes="57x57" href="' . $this->packages->getUrl('favicon/apple-icon-57x57.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="60x60" href="' . $this->packages->getUrl('favicon/apple-icon-60x60.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="72x72" href="' . $this->packages->getUrl('favicon/apple-icon-72x72.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="76x76" href="' . $this->packages->getUrl('favicon/apple-icon-76x76.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="114x114" href="' . $this->packages->getUrl('favicon/apple-icon-114x114.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="120x120" href="' . $this->packages->getUrl('favicon/apple-icon-120x120.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="144x144" href="' . $this->packages->getUrl('favicon/apple-icon-144x144.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="152x152" href="' . $this->packages->getUrl('favicon/apple-icon-152x152.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="apple-touch-icon" sizes="180x180" href="' . $this->packages->getUrl('favicon/apple-icon-180x180.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="icon" type="image/png" sizes="192x192"  href="' . $this->packages->getUrl('favicon/android-icon-192x192.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="icon" type="image/png" sizes="32x32" href="' . $this->packages->getUrl('favicon/favicon-32x32.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="icon" type="image/png" sizes="96x96" href="' . $this->packages->getUrl('favicon/favicon-96x96.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="icon" type="image/png" sizes="16x16" href="' . $this->packages->getUrl('favicon/favicon-16x16.png', 'markocupic_contao_theme_caribbean_blue') . '">
-      <link rel="manifest" href="' . $this->packages->getUrl('favicon/manifest.json', 'markocupic_contao_theme_caribbean_blue') . '">
+      <link rel="apple-touch-icon" sizes="57x57" href="'.$this->packages->getUrl('favicon/apple-icon-57x57.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="60x60" href="'.$this->packages->getUrl('favicon/apple-icon-60x60.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="72x72" href="'.$this->packages->getUrl('favicon/apple-icon-72x72.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="76x76" href="'.$this->packages->getUrl('favicon/apple-icon-76x76.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="114x114" href="'.$this->packages->getUrl('favicon/apple-icon-114x114.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="120x120" href="'.$this->packages->getUrl('favicon/apple-icon-120x120.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="144x144" href="'.$this->packages->getUrl('favicon/apple-icon-144x144.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="152x152" href="'.$this->packages->getUrl('favicon/apple-icon-152x152.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="apple-touch-icon" sizes="180x180" href="'.$this->packages->getUrl('favicon/apple-icon-180x180.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="icon" type="image/png" sizes="192x192"  href="'.$this->packages->getUrl('favicon/android-icon-192x192.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="icon" type="image/png" sizes="32x32" href="'.$this->packages->getUrl('favicon/favicon-32x32.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="icon" type="image/png" sizes="96x96" href="'.$this->packages->getUrl('favicon/favicon-96x96.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="icon" type="image/png" sizes="16x16" href="'.$this->packages->getUrl('favicon/favicon-16x16.png', 'markocupic_contao_theme_caribbean_blue').'">
+      <link rel="manifest" href="'.$this->packages->getUrl('favicon/manifest.json', 'markocupic_contao_theme_caribbean_blue').'">
       <meta name="msapplication-TileColor" content="#ffffff">
-      <meta name="msapplication-TileImage" content="' . $this->packages->getUrl('favicon/ms-icon-144x144.png', 'markocupic_contao_theme_caribbean_blue') . '">
+      <meta name="msapplication-TileImage" content="'.$this->packages->getUrl('favicon/ms-icon-144x144.png', 'markocupic_contao_theme_caribbean_blue').'">
       <meta name="theme-color" content="#ffffff">
       ';
     }
@@ -120,10 +124,25 @@ class FrontendAssetsSubscriber implements EventSubscriberInterface
         $attrNonce = !empty($nonce) ? \sprintf(' nonce="%s"', $nonce) : '';
 
         if (!empty($src)) {
-            $src = '/' . ltrim($src, '/');
+            $src = '/'.ltrim($src, '/');
         }
         $attrSrc = !empty($src) ? \sprintf(' src="%s"', $src) : '';
 
         return \sprintf('<script%s%s%s>%s</script>', $attrDefer, $attrNonce, $attrSrc, $script);
+    }
+
+    private function getTemplateName(Request $request): string|null
+    {
+        $pageModel = $request->attributes->get('pageModel');
+        if (!$pageModel instanceof PageModel) {
+            return null;
+        }
+
+        $layoutModel = $pageModel->getRelated('layout');
+        if (!$layoutModel instanceof LayoutModel) {
+            return null;
+        }
+
+        return $layoutModel->template;
     }
 }
